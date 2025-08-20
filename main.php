fb4c762df209aad1de9aa3fb254817261b2925c2
jerome652 <castillojerome171@gmail.com>
2025-08-20 13:46:56 +0000

    current exercise

--- main.php
+++ main.php
@@ -1,21 +1,75 @@
 <?php
 
-$username = "jerome";
-$password  = "secret123";
-$storepassword = "secret123";
-$isloggedin = false;
+// $username = "jerome";
+// $password  = "secret123";
+// $storepassword = "secret123";
+// $isloggedin = false;
 
-if($password === $storepassword){
-    echo"Welcome back, $username!!! <br >"; 
-    echo"You have succesfully logged in.<br >";
+// if($password === $storepassword){
+//     echo"Welcome back, $username!!! <br >"; 
+//     echo"You have succesfully logged in.<br >";
 
-    $isloggedin = true;
-    $lastlogin = date(format: "Y-m-d H:i:s");
+//     $isloggedin = true;
+//     $lastlogin = date(format: "Y-m-d H:i:s");
 
-    echo "last login: $lastlogin <br>";
+//     echo "last login: $lastlogin <br>";
 
-}
-echo "session status: ".($isloggedin?"active": "inactive")."<br>";
+// }
+// echo "session status: ".($isloggedin?"active": "inactive")."<br>";
+
+
+
+
+
+
+
+// $ordertotal = 101;
+// $firstime = false;
+// $hasloyaltycard  = false;
+
+// if($ordertotal >= 100){
+
+//     if($firstime){
+//         $discount = $ordertotal * 0.10;
+//         echo "firstime has discount of 10% <br>";
+//     }
+//     elseif ($hasloyaltycard) {
+//         $discount = $ordertotal *0.20;
+//         echo "firstime has discount of 20%<br>";
+//     }
+//     else{
+//         $discount = $ordertotal*0.05;
+//         echo "firstime has discount of 5% <br>";
+//     }
+//     $finaltotal = $discount - $ordertotal;
+
+//     echo "Original total: ".number_format(num: $ordertotal, decimals:2)."<br>";
+//     echo "discount : ".number_format(num: $discount, decimals:2)."<br>";
+//     echo "final total : ".number_format(num: $finaltotal, decimals:2)."<br>";
+      
+// }
+// else{
+    
+// }
+
+
+
+
+
+
+
+// $name ="jerome  castillo";
+// $age =17;
+// $adult = $age >= 18;
+// $teenager = $age >= 13 && $age <= 17;
+// $sinior = $age >=19;
+
+// echo "$name <br>";
+// echo "are you an adult ".($adult  ? "yes": "no")."<br>"  ;
+// echo "are you an adult ".($teenager?"yes": "no")."<br>"  ;
+//  echo "are you an adult ".($sinior ?"yes": "no")."<br>"  ;
+
+
 
 
 
